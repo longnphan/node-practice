552f79b17d7083d9413b958c8cb9474b4cec2702
Long Phan <longnphan@outlook.com>
2023-08-19 15:25:17 -0600

    add select, pagination, and numeric filters to get request

--- store/controllers/products.js
+++ store/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -18,8 +18,54 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
+
+    let result = Product.find(queryObject);
+
+    // sort
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("createAt");
+    }
+    const products = await result;
+    res.status(200).json({ products, nbHits: products.length });
   }
   console.log(queryObject);
+
+  // fields
+  if (fields) {
+    const fieldsList = sort.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  // numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b)/g;
+    let filters = numericFilters.replace(
+      regEx,
+      match => `-$operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach(item => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
